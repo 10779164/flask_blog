2db1889e0d04e3dec6ce593082cd997a620a88a4
cyaoda <cyaoda>
2016-11-01 19:52:34 +0800

    增加登入登出，注册账户，邮箱验证等功能

--- flask_test/app/main/views.py
+++ flask_test/app/main/views.py
@@ -10,20 +10,4 @@ from .. import email
 
 @main.route('/', methods=['POST','GET'])
 def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        user = User.query.filter(User.username == form.username.data).first()
-        if user is None:
-            user = User(username = form.username.data)
-            db.session.add(user)
-            session['known'] = False
-            if current_app.config['FLASK_ADMIN']:
-                email.send_email(current_app.config['FLASK_ADMIN'], 'New User',
-                                 'mail/new_user', user=user)
-        else:
-            session['known'] = True
-        session['username'] = form.username.data
-        return redirect(url_for('main.index'))
-    return render_template('index.html', form = form,
-                           username = session.get('username'),
-                           known = session.get('known', False))
+    return render_template('index.html')
--- flask_test/app/models.py
+++ flask_test/app/models.py
@@ -1,8 +1,11 @@
 #! usr/bin/env python
 # coding=utf-8
 
-from . import db
+from . import db, login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -13,15 +16,14 @@ class Role(db.Model):
     def __repr__(self):
         return '<Role %r>' % self.name
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
-
-    def __repr__(self):
-        return '<User %r>' % self.username
+    email = db.Column(db.String(64), unique=True, index=True)
+    confirmed = db.Column(db.Integer, default = 0)
 
     @property
     def password(self):
@@ -29,7 +31,35 @@ class User(db.Model):
 
     @password.setter
     def password(self, password):
+        '''生成hash密码'''
         self.password_hash = generate_password_hash(password)
 
     def verify_password(self, password):
+        '''检验密码'''
         return check_password_hash(self.password_hash, password)
+
+    def generate_confirmation_token(self, expiration = 3600):
+        '''生成一个令牌'''
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'confirm': self.id})
+
+    def confirm(self, token):
+        '''检验令牌'''
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = 1
+        db.session.add(self)
+        return True
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+# 加载用户的回调函数：接收Unicode字符串形式的用户标识符，如果有该用户就返回用户的对象，否则为None
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
